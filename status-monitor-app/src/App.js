39d127abdb497d5549ee466a3f22ebe21ed5a907
Aiden <47725379+Mewrin@users.noreply.github.com>
2021-10-05 22:34:06 +0100

    Add first text box and button
    The first text box and button to enable users to enter a website to monitor status updates for, this will probably be moved into a separate component later

--- status-monitor-app/src/App.js
+++ status-monitor-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h1 class="AppTitle"> Status Monitor </h1>
+      <input type="text" id="websiteInput"></input>
+      <input type="button" id="websiteInputSubmit" onClick={() => {
+        let inputValue = document.getElementById("websiteInput").value;
+        setValue(inputValue);
+        alert(inputValue);
+      }} value="Setup"></input>
     </div>
   );
+}
+
+
+function handleInputChange(event) {
+  
 }
 
 export default App;
