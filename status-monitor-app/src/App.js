3c405623c1dcd5563749b636f1e56ed94839af8c
Aiden <47725379+Mewrin@users.noreply.github.com>
2021-10-05 22:41:24 +0100

    now using useEffect hook to utilise callbacks when state changes

--- status-monitor-app/src/App.js
+++ status-monitor-app/src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [value, setValue] = useState("");
+  useEffect(() => {
+    if(value) {
+      console.log("value updated: " + value);
+    }
+  });
 
   return (
-    <div class="App">
-      <h1 class="AppTitle"> Status Monitor </h1>
+    <div className="App">
+      <h1 className="AppTitle"> Status Monitor </h1>
       <input type="text" id="websiteInput"></input>
       <input type="button" id="websiteInputSubmit" onClick={() => {
         let inputValue = document.getElementById("websiteInput").value;
         setValue(inputValue);
-        alert(inputValue);
       }} value="Setup"></input>
     </div>
   );
-}
-
-
-function handleInputChange(event) {
-  
 }
 
 export default App;
